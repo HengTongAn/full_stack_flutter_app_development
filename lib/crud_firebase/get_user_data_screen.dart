30752a417941b67dc0c18ecbc28cd91a2e658f65
Tong Heng <henggk64@gmail.com>
2026-02-19 19:42:55 +0700

    Flutter with Firebase set up

--- lib/crud_firebase/get_user_data_screen.dart
+++ lib/crud_firebase/get_user_data_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:full_stack_flutter_app_development/e_commerce_project/constrants/colors.dart';
 
 class GetUserDataScreen extends StatelessWidget {
   const GetUserDataScreen({super.key});
@@ -8,8 +9,11 @@ class GetUserDataScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        backgroundColor: blue,
+        elevation: 0,
         onPressed: () {
-          if (kDebugMode) print("Hello, Flutter!");
+          if (kDebugMode) print("Hello, Mae mi nheow!");
         },
         child: Icon(Icons.add),
       ),
